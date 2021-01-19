465dda13f2786a39709fb80b077512945dbc8ed3
jeishu <jeremy.zhu@utexas.edu>
2021-01-18 20:01:49 -0600

    Fixed the issue with handlebars and issues with not working with the button

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -2,59 +2,49 @@
 $(function() {
   $(".change-burger").on("click", function(event) {
     var id = $(this).data("id");
-    var newDevour = $(this).data("newdevour");
-
-    var newDevourState = {
-      devour: newDevour
-    };
 
     // Send the PUT request.
     $.ajax("/api/burgers/" + id, {
       type: "PUT",
-      data: newDevourState
+      data: {}
     }).then(
       function() {
         console.log("changed devour to", newDevour);
-        // Reload the page to get the updated list
-        location.reload();
       }
     );
+    location.href = "/";
   });
 
-  $(".create-form").on("submit", function(event) {
+  $("#submit").on("click", function(event) {
     // Make sure to preventDefault on a submit event.
     event.preventDefault();
 
-    var newBurger = {
-      burger_name: $("#bu").val().trim(),
-      devour: $("[name=devour]:checked").val().trim()
-    };
+    var newBurger = $("#bu").val().trim();
 
     // Send the POST request.
     $.ajax("/api/burgers", {
       type: "POST",
-      data: newBurger
-    }).then(
-      function() {
+      data: {burger_name: newBurger}
+    }).then(function() {
         console.log("created new burger");
-        // Reload the page to get the updated list
-        location.reload();
       }
     );
+    location.href = "/";
   });
+  
+  // location.href = "/"
+  // $(".delete-burger").on("click", function(event) {
+  //   var id = $(this).data("id");
 
-  $(".delete-burger").on("click", function(event) {
-    var id = $(this).data("id");
-
-    // Send the DELETE request.
-    $.ajax("/api/burgers/" + id, {
-      type: "DELETE"
-    }).then(
-      function() {
-        console.log("deleted burger", id);
-        // Reload the page to get the updated list
-        location.reload();
-      }
-    );
-  });
+  //   // Send the DELETE request.
+  //   $.ajax("/api/burgers/" + id, {
+  //     type: "DELETE"
+  //   }).then(
+  //     function() {
+  //       console.log("deleted burger", id);
+  //       // Reload the page to get the updated list
+  //       location.reload();
+  //     }
+  //   );
+  // });
 });
